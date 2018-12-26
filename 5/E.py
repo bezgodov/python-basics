d5d765f5c93b67413b02f9016a14cef80e8b2f0a
Max Bezgodov <bezgodov@it-etika.ru>
2018-12-27 01:21:35 +1000

    Revert "Solved some tasks from 5 day"
    This reverts commit 9bbf6a61c12a9ef88046fbd67659c2b8c684e1bc.

--- 5/E.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-N, M = list(map(int, input().split()))
-
-a = []
-for _ in range(N):
-    a.append(list(map(int, input().split())))
-
-a = np.transpose(a)
-print(np.dot(a[0], a[len(a) - 1]))
